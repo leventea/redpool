e6f62decf24db2fbcdb966715e7e514ea4e2f5c3
leventea <leventea@tuta.io>
2020-12-09 22:37:39 +0100

    Make it synchronous

--- src/redpool.nim
+++ src/redpool.nim
@@ -1,9 +1,9 @@
-import asyncdispatch, times, net
+import times, net
 import redis
 
 type
   RedisConn = ref object
-    conn: AsyncRedis
+    conn: Redis
     taken: float
 
   RedisPool* = ref object
@@ -13,14 +13,14 @@ type
     timeout: float
     maxConns: int
 
-proc newRedisConn(pool: RedisPool; taken=false): Future[RedisConn] {.async.} =
+proc newRedisConn(pool: RedisPool; taken=false): RedisConn =
   result = RedisConn(
-    conn: await openAsync(pool.host, pool.port),
+    conn: open(pool.host, pool.port),
     taken: if taken: epochTime() else: 0
   )
 
 proc newRedisPool*(size: int; maxConns=10; timeout=10.0;
-                   host="localhost"; port=6379): Future[RedisPool] {.async.} =
+                   host="localhost"; port=6379): RedisPool =
   result = RedisPool(
     maxConns: maxConns,
     host: host,
@@ -29,25 +29,25 @@ proc newRedisPool*(size: int; maxConns=10; timeout=10.0;
   )
 
   for n in 0 ..< size:
-    result.conns.add await newRedisConn(result)
+    result.conns.add newRedisConn(result)
 
-proc acquire*(pool: RedisPool): Future[AsyncRedis] {.async.} =
+proc acquire*(pool: RedisPool): Redis =
   let now = epochTime()
   for rconn in pool.conns:
     if now - rconn.taken > pool.timeout:
       rconn.taken = now
       return rconn.conn
 
-  let newConn = await newRedisConn(pool, taken=true)
+  let newConn = newRedisConn(pool, taken=true)
   pool.conns.add newConn
   return newConn.conn
 
-proc release*(pool: RedisPool; conn: AsyncRedis) {.async.} =
+proc release*(pool: RedisPool; conn: Redis) =
   for i, rconn in pool.conns:
     if rconn.conn == conn:
       if pool.conns.len > pool.maxConns:
         pool.conns.del(i)
-        await conn.quit()
+        conn.quit()
       else:
         rconn.taken = 0
       break
@@ -58,22 +58,22 @@ template withAcquire*(pool: RedisPool; conn, body: untyped) =
     try:
       body
     finally:
-      await pool.release(`conn`)
+      pool.release(`conn`)
   else:
-    let `conn` {.inject.} = waitFor pool.acquire()
+    let `conn` {.inject.} = pool.acquire()
     try:
       body
     finally:
-      waitFor pool.release(`conn`)
+      pool.release(`conn`)
 
 when isMainModule:
-  proc main {.async.} =
-    let pool = await newRedisPool(1)
-    let conn = await pool.acquire()
-    echo await conn.ping()
-    await pool.release(conn)
+  proc main =
+    let pool = newRedisPool(1)
+    let conn = pool.acquire()
+    echo conn.ping()
+    pool.release(conn)
 
     pool.withAcquire(conn2):
-      echo await conn2.ping()
+      echo conn2.ping()
 
-  waitFor main()
+  main()
